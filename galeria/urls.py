c8bcc412fb7d81d08a27c6a03cf4973ecd4bf906
gui-p <deepdholphin69@gmail.com>
2023-04-06 14:10:06 -0300

    Templates Tags, partials, base, redirecionamento

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem, surpreso
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
+    path('surpresa/', surpreso, name='surpreso')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # request é o endereço requisitado, o segundo argumento é o arquivo localizado em "templates"
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, "galeria/imagem.html")
+
+def surpreso(request):
+    return render (request, "galeria/surpresa.html")
